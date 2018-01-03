664dd879799d7c480cbe977dbd2c604514caeb7c
Definence <dp210592ybo@gmail.com>
2018-01-03 16:51:37 +0200

    complete method(with reducer)

--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -29,14 +29,17 @@ export default function tasks(state = initState, action) {
       edit: action.payload
     };
 
+  } else if (action.type === 'RESOURCES/TASKS/GET/ID/COMPLETE') {
+    //виводить масив тасків за мінусом action.payload.id
+    state.all = state.all.filter(el => el.id !== action.payload.id);
+    return {
+      //весь стейт(для того, щоб не пропав едіт)
+      ...state,
+      //action.payload - той таск, що видозмінювався
+      //state.all - масив тасків без видозміненого
+      all: [ action.payload, ...state.all ]
+    };
   }
-  // else if (action.type === 'RESOURCES/TASKS/GET/ID/COMPLETE') {
-  //   //state.tasks = state.tasks.filter(el => el.id !== action.payload.id);
-  //   return {
-  //     ...state,
-  //     all: action.payload
-  //   };
-  // }
 
   //console.log('STATE', state)
 
