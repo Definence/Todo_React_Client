7c045edb025e2b141ddd4e21173854f93ea7faa3
Definence <dp210592ybo@gmail.com>
2018-01-09 11:45:39 +0200

    log in/sign up forms

--- src/components/users/log_in.js
+++ src/components/users/log_in.js
@@ -10,7 +10,30 @@ const Log_in = () => {
 
       <Menu />
 
-      log in
+      <div className='col-sm-3 col-md-4' />
+
+      <div className='col-sm-6 col-md-4' >
+
+        <h2 className='text-center'>Log in</h2>
+
+        <form className='form-horizontal' >
+
+          <div>
+            <label>Email:</label>
+            <input className='form-control' placeholder="Enter your email" type="email" />
+          </div>
+
+          <div>
+            <label>Password:</label>
+            <input className='form-control' placeholder="Enter a password" type="password" />
+          </div>
+
+          <br />
+
+          <button type="submit" className="btn btn-primary center-block">Log in</button>
+
+        </form>
+      </div>
 
     </div>
   );
--- src/components/users/sign_up.js
+++ src/components/users/sign_up.js
@@ -10,7 +10,50 @@ const Sign_up = () => {
 
       <Menu />
 
-      sign up
+      <div className='col-sm-3 col-md-4' />
+
+      <div className='col-sm-6 col-md-4' >
+
+        <h2 className='text-center'>Sign up</h2>
+
+        <form className='form-horizontal' >
+
+          <div>
+            <label>Email:</label>
+            <input className='form-control' placeholder="Enter your email" type="email" />
+          </div>
+
+          <div>
+            <label>Username:</label>
+            <input className='form-control' placeholder="Enter a username" type="text" />
+          </div>
+
+          <div>
+            <label>Firstname:</label>
+            <input className='form-control' placeholder="Enter your firstname" type="text" />
+          </div>
+
+          <div>
+            <label>Lastname:</label>
+            <input className='form-control' placeholder="Enter your lastname" type="text" />
+          </div>
+
+          <div>
+            <label>Password:</label>
+            <input className='form-control' placeholder="Enter a password" type="password" />
+          </div>
+
+          <div>
+            <label>Password verification:</label>
+            <input className='form-control' placeholder="Enter a password" type="password" />
+          </div>
+
+          <br />
+
+          <button type="submit" className="btn btn-primary center-block">Sign up</button>
+
+        </form>
+      </div>
 
     </div>
   );
