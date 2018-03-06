b241a23a55c9d6f1f5307c6c39944105457fe406
Axife <dp210592ybo@gmail.com>
2018-03-06 15:57:17 +0200

    gfd

--- src/components/constants/api_config.js
+++ src/components/constants/api_config.js
@@ -1,9 +1,9 @@
 let URL;
 
-process.env.NODE_ENV !== 'production' ?
-  URL = 'http://localhost:3000'
-:
+process.env.NODE_ENV == 'production' ?
   URL = 'https://todo-react-api.herokuapp.com/'
+:
+  URL = 'http://localhost:3000'
 
 export const SESSION_URL = URL + '/sessions'
 
